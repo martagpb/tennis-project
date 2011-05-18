f7cc0934e452b7d3ef46eeaf51bb27c054f4de25
julien.gonzalv@gmail.com <julien.gonzalv@gmail.com@b4689b9f-f6ca-ff5d-c3b1-f603b6193608>
2011-05-18 23:56:57 +0000

    Lien    : https://tennis-project.googlecode.com/svn/trunk/604_create_pqi_ui_creneau.sql Fichier : 604_create_pqi_ui_creneau.sql Auteur  : Julien GONZALVES Date    : jeudi 18 mai 2011 vers 01h57 Type    : Création du fichier
    - Passage en minuscule du nom des méthodes pour respecter notre norme.
    - Ajout du mot "pq_" devant le nom du package.
    - Renommage du fichier pour respecter le guide du développement
    	600_create_pqi_ui_creneau.sql => 604_create_pqi_ui_creneau.sql

---
+++ trunk/604_create_pqi_ui_creneau.sql
@@ -0,0 +1,51 @@
+-- -----------------------------------------------------------------------------
+--           Création du package d'interface d'affichage des données
+--           pour la table CRENEAU
+--                      Oracle Version 10g
+--                        (10/5/2011)
+-- -----------------------------------------------------------------------------
+--      Nom de la base : Tennis
+--      Projet : Tennis_V1.24
+--      Auteur : Gonzalves / Invernizzi / Joly / Leviste
+--      Date de dernière modification : 14/05/2011
+-- -----------------------------------------------------------------------------
+
+CREATE OR REPLACE PACKAGE pq_ui_creneau
+IS
+	--Permet d'afficher tous les créneaux et les actions possibles de gestion
+	PROCEDURE manage_creneaux;
+	
+	--Permet d’afficher un créneau existant
+	PROCEDURE dis_creneau(
+	  vheureDebutCreneau IN CHAR
+	, vheureFinCreneau IN CHAR);
+	
+	-- Exécute la procédure d'ajout d'un créneau et gère les erreurs éventuelles.
+	PROCEDURE exec_add_creneau(
+	  vheureDebutCreneau IN CHAR
+	, vheureFinCreneau IN CHAR);
+	
+	-- Exécute la procédure de mise à jour d'un créneau et gère les erreurs éventuelles
+	PROCEDURE exec_upd_creneau(
+	  vheureDebutCreneau IN CHAR
+	, vheureFinCreneau IN CHAR);
+	
+	-- Exécute la procédure de suppression d'un créneau et gère les erreurs éventuelles
+	PROCEDURE exec_del_creneau(
+	  vheureDebutCreneau IN CHAR);
+	  
+	-- Exécute la procédure d’affichage des créneaux et gère les erreurs éventuelles
+	PROCEDURE exec_dis_creneau(
+	  vheureDebutCreneau IN CHAR
+	, vheureFinCreneau IN CHAR);
+	
+	-- Affiche le formulaire permettant la saisie d’un nouveau créneau
+	PROCEDURE form_add_creneau;
+	
+	-- Affiche le formulaire de saisie permettant la modification d’un créneau existant
+	PROCEDURE form_upd_creneau(
+	  vheureDebutCreneau IN CHAR
+	, vheureFinCreneau IN CHAR);
+	
+END pq_ui_creneau;
+/
