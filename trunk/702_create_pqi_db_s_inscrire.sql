cfccfc3d75331523722dfb23b93c24eee0423ac4
julien.gonzalv@gmail.com <julien.gonzalv@gmail.com@b4689b9f-f6ca-ff5d-c3b1-f603b6193608>
2011-05-19 00:15:48 +0000

    Lien    : https://tennis-project.googlecode.com/svn/trunk/702_create_pqi_db_s_inscrire.sql Fichier : 702_create_pqi_db_s_inscrire.sql Auteur  : Julien GONZALVES Date    : jeudi 19 mai 2011 vers 02h16 Type    : Création du fichier
    - Passage en minuscule du nom des méthodes pour respecter notre norme.
    - Ajout du mot "pq_" devant le nom du package.
    - Renommage du fichier pour respecter le guide du développement
    	700_create_pqi_db_s_inscrire.sql => 702_create_pqi_db_s_inscrire.sql

---
+++ trunk/702_create_pqi_db_s_inscrire.sql
@@ -0,0 +1,31 @@
+-- -----------------------------------------------------------------------------
+--           Création du package d'interface d'accès à la base de données 
+--           pour la table S_INSCRIRE
+--                      Oracle Version 10g
+--                        (10/5/2011)
+-- -----------------------------------------------------------------------------
+--      Nom de la base : Tennis
+--      Projet : Tennis_V1.24
+--      Auteur : Gonzalves / Invernizzi / Joly / Leviste
+--      Date de dernière modification : 14/05/2011
+-- -----------------------------------------------------------------------------
+
+CREATE OR REPLACE PACKAGE pq_db_s_inscrire
+IS
+	--Permet d’ajouter une inscription
+	PROCEDURE add_inscription(
+	  vnumEntrainement IN NUMBER
+	, vnumPersonne IN NUMBER);
+	
+	--Permet de modifier une inscription existante
+	PROCEDURE upd_inscription(
+	  vnumEntrainement IN NUMBER
+	, vnumPersonne IN NUMBER);
+	
+	--Permet de supprimer une inscription existante
+	PROCEDURE del_inscription(
+	  vnumEntrainement IN NUMBER
+	, vnumPersonne IN NUMBER);
+		
+END pq_db_s_inscrire;
+/
