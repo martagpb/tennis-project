3079dd713eec863e48d6f67b8ee740d05d29a5f8
julien.gonzalv@gmail.com <julien.gonzalv@gmail.com@b4689b9f-f6ca-ff5d-c3b1-f603b6193608>
2011-05-19 00:24:49 +0000

    Lien    : https://tennis-project.googlecode.com/svn/trunk/create.js Fichier : create.js Auteur  : Julien GONZALVES Date    : jeudi 19 mai 2011 vers 02h25 Type    : Mise à jour du fichier
    - Ajout de la méthode validerCreneau permettant de valider la sélection d'un créneau horaire ;
    - Ajout de la méthode confirmerChoix permettant de déterminer si un utilisateur confirmer son choix ;
    - Ajout de commentaires en en-tête du fichier (auteur, version, date, etc.).

--- trunk/create.js
+++ trunk/create.js
@@ -1,4 +1,11 @@
-
+/*
+-- -----------------------------------------------------------------------------
+--		Type   : Fichier javascript
+--      Projet : BD50
+--      Auteur : Gonzalves / Invernizzi / Joly / Leviste
+--      Date de dernière modification : 18/05/2011
+-- -----------------------------------------------------------------------------
+*/
 
 function valider(form,document){
 
@@ -35,6 +42,31 @@ function valider(form,document){
 }
 
 function verifiermail(mail) {
-      return (mail.indexOf("@")>=0)&&(mail.indexOf(".")>=0);
-   }
-   
+   return (mail.indexOf("@")>=0)&&(mail.indexOf(".")>=0);
+}
+
+/*Fonction permettant de valider la sélection d'un créneau horaire*/
+function validerCreneau(form,document){
+
+	heureDebutCreneau = form.vheureDebutCreneau.value;
+	
+	if(heureDebutCreneau == null || heureDebutCreneau == ""){
+		alert("Veuillez remplir le champ de l'heure de début du créneau.");
+		return false;
+	}
+	
+	//TODO : vérifier que l'heure de début du créneau est strictement inférieure à l'heure de fin si elle existe.
+		// Si heure de début strictement inférieure à heure de fin : OK ;
+		// Si heure de début identique à heure de fin : false ;
+		// Si heure de début strictement supérieure à heure de fin : inverser les heures de début et de fin.
+		
+	return true;
+}
+
+/*Fonction permettant de déterminer si un utilisateur confirmer son choix*/
+function confirmerChoix(form,document){
+  if (confirm("Confirmez-vous votre décision ?")) {
+    return true;
+  }
+   return false;
+}
