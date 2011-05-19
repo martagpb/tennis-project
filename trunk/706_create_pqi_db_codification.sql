7006b20c3aef84366685e8053930750187103d9c
julien.gonzalv@gmail.com <julien.gonzalv@gmail.com@b4689b9f-f6ca-ff5d-c3b1-f603b6193608>
2011-05-19 00:19:46 +0000

    Lien    : https://tennis-project.googlecode.com/svn/trunk/706_create_pqi_db_codification.sql Fichier : 706_create_pqi_db_codification.sql Auteur  : Julien GONZALVES Date    : jeudi 19 mai 2011 vers 02h20 Type    : Création du fichier
    - Passage en minuscule du nom des méthodes pour respecter notre norme.
    - Ajout du mot "pq_" devant le nom du package.
    - Renommage du fichier pour respecter le guide du développement
    	700_create_pqi_db_codification.sql => 706_create_pqi_db_codification.sql

---
+++ trunk/706_create_pqi_db_codification.sql
@@ -0,0 +1,33 @@
+-- -----------------------------------------------------------------------------
+--           Création du package d'interface d'accès à la base de données 
+--           pour la table CODIFICATION
+--                      Oracle Version 10g
+--                        (10/5/2011)
+-- -----------------------------------------------------------------------------
+--      Nom de la base : Tennis
+--      Projet : Tennis_V1.24
+--      Auteur : Gonzalves / Invernizzi / Joly / Leviste
+--      Date de dernière modification : 14/05/2011
+-- -----------------------------------------------------------------------------
+
+CREATE OR REPLACE PACKAGE pq_db_codification
+IS
+	--Permet d’ajouter une codification
+	PROCEDURE add_codification(
+	  vcode IN CHAR
+	, vnature IN VARCHAR2
+	, vlibelle IN VARCHAR2);	
+	
+	--Permet de modifier une codification existante
+	PROCEDURE upd_codification(
+	  vcode IN CHAR
+	, vnature IN VARCHAR2
+	, vlibelle IN VARCHAR2);	
+	
+	--Permet de supprimer une codification existante
+	PROCEDURE del_codification(
+	  vcode IN CHAR
+	, vnature IN VARCHAR2);
+	
+END pq_db_codification;
+/
