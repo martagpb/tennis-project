259c672abc46a87c675c4e9039ad9268973335c5
julien.gonzalv@gmail.com <julien.gonzalv@gmail.com@b4689b9f-f6ca-ff5d-c3b1-f603b6193608>
2011-05-19 00:03:43 +0000

    Lien    : https://tennis-project.googlecode.com/svn/trunk/608_create_pqi_ui_entrainement.sql Fichier : 608_create_pqi_ui_entrainement.sql Auteur  : Julien GONZALVES Date    : jeudi 19 mai 2011 vers 02h05 Type    : Création du fichier
    - Passage en minuscule du nom des méthodes pour respecter notre norme.
    - Ajout du mot "pq_" devant le nom du package.
    - Renommage du fichier pour respecter le guide du développement
    	600_create_pqi_ui_entrainement.sql => 608_create_pqi_ui_entrainement.sql

---
+++ 608_create_pqi_ui_entrainement.sql
@@ -0,0 +1,78 @@
+-- -----------------------------------------------------------------------------
+--           Création du package d'interface d'affichage des données
+--           pour la table ENTRAINEMENT
+--                      Oracle Version 10g
+--                        (10/05/2011)
+-- -----------------------------------------------------------------------------
+--      Nom de la base : Tennis
+--      Projet : Tennis_V1.24
+--      Auteur : Gonzalves / Invernizzi / Joly / Leviste
+--      Date de dernière modification : 14/05/2011
+-- -----------------------------------------------------------------------------
+
+CREATE OR REPLACE PACKAGE pq_ui_entrainement
+IS
+	--Permet d’afficher un entrainement existant
+	PROCEDURE dis_entrainement(
+	  vnumEntrainement IN NUMBER
+	, vnumEmploye IN NUMBER
+	, vcodeNiveau IN CHAR
+	, vnatureNiveau IN VARCHAR2
+	, vnbPlaces IN NUMBER
+	, vdateDebut IN DATE
+	, vdateFin IN DATE
+	, vestRecurent IN NUMBER);
+	
+	-- Exécute la procédure d'ajout d'un entrainement et gère les erreurs éventuelles.
+	PROCEDURE exec_add_entrainement(
+	  vnumEntrainement IN NUMBER
+	, vnumEmploye IN NUMBER
+	, vcodeNiveau IN CHAR
+	, vnatureNiveau IN VARCHAR2
+	, vnbPlaces IN NUMBER
+	, vdateDebut IN DATE
+	, vdateFin IN DATE
+	, vestRecurent IN NUMBER);
+	
+	-- Exécute la procédure de mise à jour d'un entrainement et gère les erreurs éventuelles
+	PROCEDURE exec_upd_entrainement(
+	  vnumEntrainement IN NUMBER
+	, vnumEmploye IN NUMBER
+	, vcodeNiveau IN CHAR
+	, vnatureNiveau IN VARCHAR2
+	, vnbPlaces IN NUMBER
+	, vdateDebut IN DATE
+	, vdateFin IN DATE
+	, vestRecurent IN NUMBER);
+
+	-- Exécute la procédure de suppression d'un entrainement et gère les erreurs éventuelles
+	PROCEDURE exec_del_entrainement(
+	  vnumEntrainement IN NUMBER);
+	
+	-- Exécute la procédure d’affichage des entrainements et gère les erreurs éventuelles
+	PROCEDURE exec_dis_entrainement(
+	  vnumEntrainement IN NUMBER
+	, vnumEmploye IN NUMBER
+	, vcodeNiveau IN CHAR
+	, vnatureNiveau IN VARCHAR2
+	, vnbPlaces IN NUMBER
+	, vdateDebut IN DATE
+	, vdateFin IN DATE
+	, vestRecurent IN NUMBER);
+	
+	-- Affiche le formulaire permettant la saisie d’un nouvel entrainement
+	PROCEDURE form_add_entrainement;
+	
+	-- Affiche le formulaire de saisie permettant la modification d’un entrainement existant	
+	PROCEDURE form_upd_entrainement(
+	  vnumEntrainement IN NUMBER
+	, vnumEmploye IN NUMBER
+	, vcodeNiveau IN CHAR
+	, vnatureNiveau IN VARCHAR2
+	, vnbPlaces IN NUMBER
+	, vdateDebut IN DATE
+	, vdateFin IN DATE
+	, vestRecurent IN NUMBER);
+		
+END pq_ui_entrainement;
+/
