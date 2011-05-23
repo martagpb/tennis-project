d83e6e21bc6fb0000f3be86733028061094a627f
david.invernizzi25@gmail.com <david.invernizzi25@gmail.com@b4689b9f-f6ca-ff5d-c3b1-f603b6193608>
2011-05-23 21:59:07 +0000

    modification de 622_create_pqb_ui_avoir_lieu.sql et 622_create_pqi_ui_avoir_lieu.sql

---
+++ trunk/622_create_pqi_ui_avoir_lieu.sql
@@ -0,0 +1,56 @@
+-- -----------------------------------------------------------------------------
+--           Création du package d'interface d'affichage des données
+--           pour la table AVOIR_LIEU
+--                      Oracle Version 10g
+--                        (10/05/2011)
+-- -----------------------------------------------------------------------------
+--      Nom de la base : Tennis
+--      Projet : Tennis_V1.24
+--      Auteur : Gonzalves / Invernizzi / Joly / Leviste
+--      Date de dernière modification : 14/05/2011
+-- -----------------------------------------------------------------------------
+
+CREATE OR REPLACE PACKAGE pq_ui_avoir_lieu
+IS
+	
+	-- Exécute la procédure d'ajout d'une séance d'un entrainement et gère les erreurs éventuelles.
+	PROCEDURE exec_add_avoir_lieu(
+	  vnumJour IN NUMBER
+	, vheureDebutCreneau IN CHAR
+	, vnumTerrain IN NUMBER
+	, vnumEntrainement IN NUMBER);
+	
+	-- Exécute la procédure de mise à jour d'une séance d'un entrainement et gère les erreurs éventuelles.
+	PROCEDURE exec_upd_avoir_lieu(
+	  vnumJour IN NUMBER
+	, vheureDebutCreneau IN CHAR
+	, vnumTerrain IN NUMBER
+	, vnumEntrainement IN NUMBER);
+	
+	-- Exécute la procédure de suppression d'une séance d'un entrainement et gère les erreurs éventuelles
+	PROCEDURE exec_del_avoir_lieu(
+	  vnumJour IN NUMBER
+	, vheureDebutCreneau IN CHAR
+	, vnumTerrain IN NUMBER
+	, vnumEntrainement IN NUMBER);
+	  
+	-- Affiche le formulaire permettant la saisie d'une nouvelle séance d'un entrainement
+	PROCEDURE form_add_avoir_lieu(
+	  vnumEntrainement IN NUMBER
+	, vnumEntraineur IN NUMBER
+	, vcodeNiveau IN CHAR
+	, vnatureNiveau IN VARCHAR2
+	, vnbPlaces IN NUMBER
+	, vdateDebut IN DATE
+	, vdateFin IN DATE
+	, vestRecurent IN NUMBER);
+	
+	-- Affiche le formulaire de saisie permettant la modification d’un entrainement existant	
+	PROCEDURE form_upd_avoir_lieu(
+	  vnumJour IN NUMBER
+	, vheureDebutCreneau IN CHAR
+	, vnumTerrain IN NUMBER
+	, vnumEntrainement IN NUMBER);
+		
+END pq_ui_avoir_lieu;
+/
