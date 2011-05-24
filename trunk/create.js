bd5279f90f9c7aa3fe45abde6ab9a67a2be1fea4
julien.gonzalv@gmail.com <julien.gonzalv@gmail.com@b4689b9f-f6ca-ff5d-c3b1-f603b6193608>
2011-05-24 01:35:21 +0000

    Lien    : https://tennis-project.googlecode.com/svn/trunk/create.js Fichiers: create.js Auteur  : Julien GONZALVES Date    : Mardi 24 mai 2011 vers 03h04 Type    : Mise à jour
    - Ajout de la procédure javascript "validerCreneau" qui permet de valider la création d'un créneau
    	-- On construit deux dates fictives pour comparer facilement le créneau de début et le créneau de fin
    	-- On compare les deux dates en récupérant le nombre de millisecondes écoulés depuis le 1er janvier 1970 pour chacune des deux dates grâce à la méthode "getTime()"
    		--- Si la différence, des deux dates, est supérieure ou égale à 0 c'est que l'heure de début est supérieure à l'heure de fin du créneau
    		 ---- On affiche alors un message explicite à l'utilisateur
    		--- Sinon, si la différence est négative alors le créneau est valide et il est donc créé.
    - Ajout de commentaires en en-tête du fichier javascript pour détailler les propriétés du fichier (nom, version, date, auteurs).

--- trunk/create.js
+++ trunk/create.js
@@ -1,11 +1,9 @@
-/*
--- -----------------------------------------------------------------------------
---		Type   : Fichier javascript
---      Projet : BD50
---      Auteur : Gonzalves / Invernizzi / Joly / Leviste
---      Date de dernière modification : 21/05/2011
--- -----------------------------------------------------------------------------
-*/
+/*------------------------------------------------------------
+|	   Type   : Fichier javascript							  |
+|      Projet : BD50										  |
+|      Auteur : Gonzalves / Invernizzi / Joly / Leviste	      |
+|      Date de dernière modification : 23/05/2011		      |
+-------------------------------------------------------------*/
 
 sfHover = function() {
              var sfEls = document.getElementsByTagName("li");
@@ -57,24 +55,6 @@ function verifiermail(mail) {
    return (mail.indexOf("@")>=0)&&(mail.indexOf(".")>=0);
 }
 
-/*Fonction permettant de valider la création d'un créneau horaire*/
-function validerCreneau(form,document){
-
-	heureDebutCreneau = form.vheureDebutCreneau.value;
-	
-	if(heureDebutCreneau == null || heureDebutCreneau == ""){
-		alert("Veuillez remplir le champ de l'heure de début du créneau.");
-		return false;
-	}
-	
-	//TODO : vérifier que l'heure de début du créneau est strictement inférieure à l'heure de fin si elle existe.
-		// Si heure de début strictement inférieure à heure de fin : OK ;
-		// Si heure de début identique à heure de fin : false ;
-		// Si heure de début strictement supérieure à heure de fin : inverser les heures de début et de fin.
-		
-	return true;
-}
-
 /*Fonction permettant de déterminer si un utilisateur confirmer son choix*/
 function confirmerChoix(form,document){
   if (confirm("Confirmez-vous votre décision ?")) {
@@ -116,5 +96,105 @@ function validerTerrain(form,document){
 		//document.getElementById("vlibelleSurface").value = vLibelle;
 	}
 		
+	return true;
+}
+
+/*Fonction permettant de valider la création d'un créneau*/
+function validerCreneau(form,document){
+  
+  //Récupération des heures et minutes de début de fin du créneau
+  heureDebutCreneau  = form.vheureDebut.value;
+  minuteDebutCreneau = form.vminuteDebut.value;
+  heureFinCreneau    = form.vheureFin.value;
+  minuteFinCreneau   = form.vminuteFin.value;
+    
+  if(  
+	       heureDebutCreneau == null 
+	   ||  heureDebutCreneau == "" 
+	   ||  minuteDebutCreneau == null 
+	   ||  minuteDebutCreneau == "" 	   	  
+	   ||  heureFinCreneau == "" 	   	   
+	   ||  heureFinCreneau == "" 
+	   ||  minuteFinCreneau == null 
+	   ||  minuteFinCreneau == "" 
+	){		
+		alert("Veuillez remplir tous les champs obligatoires");
+		return false;
+	}else{
+		//Création de deux dates fictives pour comparer facilement le début et la fin du créneau
+		var year   = 1970;
+		var month  = 0;
+		var day    = 0;
+		var second = 0;
+		
+		var dateDebutCreneau = new Date ( year, month, day , heureDebutCreneau, minuteDebutCreneau, second );
+		var dateFinCreneau = new Date ( year, month, day , heureFinCreneau, minuteFinCreneau, second );
+		
+		//On comparaison les deux dates en récupérant le nombre de millisecondes écoulés depuis le 1er janvier 1970 pour chacune des deux dates
+		var difference = dateDebutCreneau.getTime() - dateFinCreneau.getTime();
+		
+		//Si la différence est supérieure ou égale à 0 c'est que l'heure de début est supérieure à l'heure de fin du créneau
+		if(difference >= 0){
+			document.getElementById("vheureDebutError").innerHTML ="L'heure de début doit être strictement inférieure à l'heure de fin.";
+			document.getElementById("vheureFinError").innerHTML ="L'heure de fin doit être strictement supérieure à l'heure de début.";
+			return false;
+		}
+		//Sinon, si la différence est négative alors le créneau est valide. On met donc à jour les champs "hidden" du formulaire qui vont contenir les heures du début et de fin du créneau
+		else{
+			var separateur = "h";
+			document.getElementById("idVheureDebutCreneau").value = heureDebutCreneau+separateur+minuteDebutCreneau; 	
+			document.getElementById("idVheureFinCreneau").value = heureFinCreneau+separateur+minuteFinCreneau;		 
+		}
+	}
+	//On retroune vrai si le créneau est valide
+	return true;
+}
+
+/*Fonction permettant de valider la création d'un créneau*/
+function validerMAJCreneau(form,document){
+  
+  //Récupération des heures et minutes de début de fin du créneau
+  heureDebutCreneau  = form.vheureDebut.value;
+  minuteDebutCreneau = form.vminuteDebut.value;
+  heureFinCreneau    = form.vheureFin.value;
+  minuteFinCreneau   = form.vminuteFin.value;
+
+  if(  
+	       heureDebutCreneau == null 
+	   ||  heureDebutCreneau == "" 
+	   ||  minuteDebutCreneau == null 
+	   ||  minuteDebutCreneau == "" 	   	  
+	   ||  heureFinCreneau == "" 	   	   
+	   ||  heureFinCreneau == "" 
+	   ||  minuteFinCreneau == null 
+	   ||  minuteFinCreneau == "" 
+	){		
+		alert("Veuillez remplir tous les champs obligatoires");
+		return false;
+	}else{
+		//Création de deux dates fictives pour comparer facilement le début et la fin du créneau
+		var year   = 1970;
+		var month  = 0;
+		var day    = 0;
+		var second = 0;
+		
+		var dateDebutCreneau = new Date ( year, month, day , heureDebutCreneau, minuteDebutCreneau, second );
+		var dateFinCreneau = new Date ( year, month, day , heureFinCreneau, minuteFinCreneau, second );
+		
+		//On comparaison les deux dates en récupérant le nombre de millisecondes écoulés depuis le 1er janvier 1970 pour chacune des deux dates
+		var difference = dateDebutCreneau.getTime() - dateFinCreneau.getTime();
+		
+		//Si la différence est supérieure ou égale à 0 c'est que l'heure de début est supérieure à l'heure de fin du créneau
+		if(difference >= 0){
+			document.getElementById("vheureFinError").innerHTML ="L'heure de fin doit être strictement supérieure à l'heure de début.";
+			return false;
+		}
+		//Sinon, si la différence est négative alors le créneau est valide. On met donc à jour le champ "hidden" du formulaire qui va contenir l'heure de fin du créneau
+		else{
+			var separateur = "h";	
+			document.getElementById("idVheureFinCreneau").value = heureFinCreneau+separateur+minuteFinCreneau;		 
+		}
+	}
+	//On retroune vrai si le créneau est valide
 	return true;
 }
