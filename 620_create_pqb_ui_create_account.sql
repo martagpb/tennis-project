7ebcdbdc962531611deafc7dd85d6ef3e3426654
julien.gonzalv@gmail.com <julien.gonzalv@gmail.com@b4689b9f-f6ca-ff5d-c3b1-f603b6193608>
2011-06-05 03:20:10 +0000

    Lien    : https://tennis-project.googlecode.com/svn/trunk/620_create_pqb_ui_create_account.sql Fichier : 620_create_pqb_ui_create_account.sql Auteur  : Julien GONZALVES Date    : Dimanche 5 juin 2011 vers 5h19 Type    : Mise à jour
    - Modification de la procédure "create_account" pour 
    	-- Afficher correctement les formulaires de connexion et de création de compte ;
    	-- Gérer les exceptions suivantes : "Un compte existe déjà avec le même login" et "La taille du mot de passe n''est pas correcte."
    - Ajout de la classe "titre_niveau_1" pour tous les titres ;
    - Ajout de la classe "success" pour tous les messages de confirmation ;
    - Remplacement du mot "AS" par "IS" puisque le mot "IS" est utilisé dans tous les fichiers.

--- 620_create_pqb_ui_create_account.sql
+++ 620_create_pqb_ui_create_account.sql
@@ -11,88 +11,97 @@
  
  
 CREATE OR REPLACE PACKAGE BODY pq_ui_create_account
-AS 
+IS  
 	PROCEDURE formCreate IS
-	begin
-		pq_ui_commun.aff_header;
-			htp.br;
-			htp.print('Création d''un nouveau compte joueur');
-			htp.br;
-			htp.print('Les champs marqués d''une étoile sont obligatoires');
-			htp.br;
-			htp.formOpen(owa_util.get_owa_service_path ||  'pq_ui_create_account.create_account', 'POST', cattributes => 'onSubmit="return valider(this,document)"');
-				htp.tableOpen(cattributes => 'CELLSPACING=8');
-					htp.tableheader('');
-					htp.tableheader('');
-					htp.tableheader('');
-					htp.tableRowOpen;
-						htp.tableData('Nom * :', cattributes => 'class="enteteFormulaire"');
-						htp.tableData(htf.formText('lastname',20));
-						htp.tableData('',cattributes => 'id="lastnameText" class="error"');
-					htp.tableRowClose;	
-					htp.tableRowOpen;
-						htp.tableData('Prénom * :', cattributes => 'class="enteteFormulaire"');
-						htp.tableData(htf.formText('firstname',20));
-						htp.tableData('',cattributes => 'id="firstnameText" class="error"');
-					htp.tableRowClose;
-					htp.tableRowOpen;
-						htp.tableData('Identifiant * :', cattributes => 'class="enteteFormulaire"');
-						htp.tableData(htf.formText('login',20));
-						htp.tableData('',cattributes => 'id="identifiantText" class="error"');
-					htp.tableRowClose;
-					htp.tableRowOpen;
-						htp.tableData('Mot de passe * :', cattributes => 'class="enteteFormulaire"');
-						htp.tableData(htf.formPassword('password',20));
-						htp.tableData('',cattributes => 'id="passwordText" class="error"');
-					htp.tableRowClose;
-					htp.tableRowOpen;
-						htp.tableData('Adresse mail * :', cattributes => 'class="enteteFormulaire"');
-						htp.tableData(htf.formText('mail',20));
-						htp.tableData('',cattributes => 'id="mailText" class="error"');
-					htp.tableRowClose;
-					htp.tableRowOpen;
-						htp.tableData('Téléphone :', cattributes => 'class="enteteFormulaire"');
-						htp.tableData(htf.formText('phone',20));
-						htp.tableData('',cattributes => 'id="phoneText" class="error"');
-					htp.tableRowClose;
-					htp.tableRowOpen;
-						htp.tableData('Adresse :', cattributes => 'class="enteteFormulaire"');
-						htp.tableData(htf.formText('street',20));
-						htp.tableData('');
-					htp.tableRowClose;
-					htp.tableRowOpen;
-						htp.tableData('Code postal :', cattributes => 'class="enteteFormulaire"');
-						htp.tableData(htf.formText('postal',20));
-						htp.tableData('',cattributes => 'id="postalText" class="error"');
-					htp.tableRowClose;
-					htp.tableRowOpen;
-						htp.tableData('Ville :', cattributes => 'class="enteteFormulaire"');
-						htp.tableData(htf.formText('city',20));
-						htp.tableData('',cattributes => 'id="cityText" class="error"');
-					htp.tableRowClose;
-					htp.tableRowOpen;
-						htp.tableData('');
-						htp.tableData(htf.formSubmit(NULL,'Validation'));
-					htp.tableRowClose;
-				htp.tableClose();
-			htp.formClose;
-			htp.br;
-			htp.anchor('pq_ui_login.login', 'Retourner à l''accueil');
-			pq_ui_commun.aff_footer;
+	begin			
+		htp.print('<div class="titre_niveau_1">');
+			htp.print('Création d''un nouveau compte');
+		htp.print('</div>');			
+		htp.br;
+		htp.print('Les champs marqués d''une étoile sont obligatoires');
+		htp.br;
+		htp.formOpen(owa_util.get_owa_service_path ||  'pq_ui_create_account.create_account', 'POST', cattributes => 'onSubmit="return valider(this,document)"');
+			htp.tableOpen(cattributes => 'CELLSPACING=8');
+				htp.tableheader('');
+				htp.tableheader('');
+				htp.tableheader('');
+				htp.tableRowOpen;
+					htp.tableData('Nom * :', cattributes => 'class="enteteFormulaire"');
+					htp.tableData(htf.formText('lastname',20));
+					htp.tableData('',cattributes => 'id="lastnameText" class="error"');
+				htp.tableRowClose;	
+				htp.tableRowOpen;
+					htp.tableData('Prénom * :', cattributes => 'class="enteteFormulaire"');
+					htp.tableData(htf.formText('firstname',20));
+					htp.tableData('',cattributes => 'id="firstnameText" class="error"');
+				htp.tableRowClose;
+				htp.tableRowOpen;
+					htp.tableData('Identifiant * :', cattributes => 'class="enteteFormulaire"');
+					htp.tableData(htf.formText('login',20));
+					htp.tableData('',cattributes => 'id="identifiantText" class="error"');
+				htp.tableRowClose;
+				htp.tableRowOpen;
+					htp.tableData('Mot de passe * :', cattributes => 'class="enteteFormulaire"');
+					htp.tableData(htf.formPassword('password',20));
+					htp.tableData('',cattributes => 'id="passwordText" class="error"');
+				htp.tableRowClose;
+				htp.tableRowOpen;
+					htp.tableData('Adresse mail * :', cattributes => 'class="enteteFormulaire"');
+					htp.tableData(htf.formText('mail',20));
+					htp.tableData('',cattributes => 'id="mailText" class="error"');
+				htp.tableRowClose;
+				htp.tableRowOpen;
+					htp.tableData('Téléphone :', cattributes => 'class="enteteFormulaire"');
+					htp.tableData(htf.formText('phone',20));
+					htp.tableData('',cattributes => 'id="phoneText" class="error"');
+				htp.tableRowClose;
+				htp.tableRowOpen;
+					htp.tableData('Adresse :', cattributes => 'class="enteteFormulaire"');
+					htp.tableData(htf.formText('street',20));
+					htp.tableData('');
+				htp.tableRowClose;
+				htp.tableRowOpen;
+					htp.tableData('Code postal :', cattributes => 'class="enteteFormulaire"');
+					htp.tableData(htf.formText('postal',20));
+					htp.tableData('',cattributes => 'id="postalText" class="error"');
+				htp.tableRowClose;
+				htp.tableRowOpen;
+					htp.tableData('Ville :', cattributes => 'class="enteteFormulaire"');
+					htp.tableData(htf.formText('city',20));
+					htp.tableData('',cattributes => 'id="cityText" class="error"');
+				htp.tableRowClose;
+				htp.tableRowOpen;
+					htp.tableData('');
+					htp.tableData(htf.formSubmit(NULL,'Validation'));
+				htp.tableRowClose;
+			htp.tableClose();
+		htp.formClose;
+		htp.br;		
 	END;
 	
 	PROCEDURE create_account ( lastname IN VARCHAR2,  firstname IN VARCHAR2,login IN VARCHAR2,password IN VARCHAR2,mail IN VARCHAR2,phone IN VARCHAR2,street IN VARCHAR2,postal IN VARCHAR2,city IN VARCHAR2)
 	IS
 	BEGIN 
-	pq_ui_commun.aff_header;
-		htp.br; 
-		  PA_PERSONNE.createPersonne(lastname,firstname,login,password,mail,phone,street,postal,city); 
-		  htp.print('Compte créé');
-		  htp.anchor('pq_ui_login.login', 'Retour');
-		pq_ui_commun.aff_footer;
-		EXCEPTION 
+	pq_ui_commun.header;
+		htp.div(cattributes => 'id="corps"');
+			htp.br; 
+			pq_db_personne.createPersonne(lastname,firstname,login,password,mail,phone,street,postal,city); 
+			htp.print('<div class="success"> ');
+				htp.print('Compte créé avec succès.');
+			htp.print('</div>');
+			htp.br;
+			--On affiche les formulaires de connexion et de création de compte
+			pq_ui_login.aff_login;
+	pq_ui_commun.aff_footer;
+	EXCEPTION 
+		WHEN DUP_VAL_ON_INDEX THEN
+				pq_ui_commun.dis_error_custom('Le compte n''a pas été créé','Un compte existe déjà avec le login '|| login ||'.','Merci de choisir une autre valeur pour le login.','pq_ui_login.login','Retour vers la création d''un nouveau compte');
 		WHEN OTHERS THEN
-		  pq_ui_commun.dis_error(TO_CHAR(SQLCODE),SQLERRM,'Création d''un compte');
-	END ; 
+			IF (SQLCODE=-28232) THEN
+				pq_ui_commun.dis_error_custom('Le compte n''a pas été créé','La taille du mot de passe n''est pas correcte.','Merci de choisir un mot de passe qui se compose de 8 caractères ou d''un multiple de 8.','pq_ui_login.login','Retour vers la création d''un nouveau compte');
+			ELSE
+				pq_ui_commun.dis_error(TO_CHAR(SQLCODE),SQLERRM,'Création d''un compte');
+			END IF;
+	END; 
 END pq_ui_create_account;
 /
