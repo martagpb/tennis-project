a78eb66c48c5b3c2a4e2197f15807362178a6653
julien.gonzalv@gmail.com <julien.gonzalv@gmail.com@b4689b9f-f6ca-ff5d-c3b1-f603b6193608>
2011-06-11 16:03:35 +0000

    Lien    : https://tennis-project.googlecode.com/svn/trunk/601_create_pqb_ui_accueil.sql Fichier : 601_create_pqb_ui_accueil.sql Auteur  : Julien GONZALVES Date    : Samedi 11 juin 2011 vers 18h03 Type    : Mise à jour
    - Réécriture complète du contenu de la procédure "dis_accueil" qui permet d'afficher la page d'accueil ;
    - Niveau de droit nécessaire pour accéder à la page : 0.

--- trunk/601_create_pqb_ui_accueil.sql
+++ trunk/601_create_pqb_ui_accueil.sql
@@ -1,37 +1,48 @@
 -- -----------------------------------------------------------------------------
---           Création du package d'interface d'affichage des données
---           pour la table ENTRAINEMENT
+--     Création du package d'interface d'affichage de l'accueil
 --                      Oracle Version 10g
 --                        (10/05/2011)
 -- -----------------------------------------------------------------------------
 --      Nom de la base : Tennis
 --      Projet : Tennis_V1.24
 --      Auteur : Gonzalves / Invernizzi / Joly / Leviste
---      Date de dernière modification : 14/05/2011
+--      Date de dernicre modification : 14/05/2011
 -- -----------------------------------------------------------------------------
 
 CREATE OR REPLACE PACKAGE BODY pq_ui_accueil
 IS
-
 	--affichage de la page d'accueil
 	PROCEDURE dis_accueil
 	IS
-		rep_img VARCHAR2(255) := pq_ui_param_commun.get_rep_img;
+		perm BOOLEAN;
+		PERMISSION_DENIED EXCEPTION;
 	BEGIN
+        pq_ui_commun.ISAUTHORIZED(niveauP=>0,permission=>perm);
+		IF perm=false THEN
+			RAISE PERMISSION_DENIED;
+		END IF;
 		pq_ui_commun.aff_header;
 		htp.br;
 		htp.br;
-		htp.Print('BIENVENUE SUR LE SITE DU CENTRE DE TENNIS DE SAINT GILLES CROIX DE VIE LES SAPINS');
+		htp.print('<div class="titre_niveau_1">');
+			htp.print('Bienvenue sur l''application de réservation de votre centre de tennis');
+		htp.print('</div>');		
 		htp.br;
 		htp.br;
-		htp.print('<img title="Accueil" alt="image" class="accueil" src="' || rep_img || 'accueil.jpg">');	
+		htp.print('Le rôle principal de cette application est de permettre la gestion des réservations.');
 		htp.br;
+		htp.print('Elle est principalement destinée à être utilisée par les agents d''accueil et les entraineurs du club.');	
 		htp.br;
+		htp.print('Par ailleurs, l''application a pour objectif d''améliorer et de simplifier les tâches classiques de gestion dans le but de faire gagner du temps aux utilisateurs.');
+		htp.br;
+		htp.print('Enfin, cette application va permettre de gérer et de recenser les joueurs, les réservations, les entraineurs, les abonnements, les factures et les terrains du club.');
+		htp.br;		
 		htp.br;
 		pq_ui_commun.aff_footer;
-	END;
-	
-	
+	EXCEPTION
+		WHEN PERMISSION_DENIED THEN
+			pq_ui_commun.dis_error_permission_denied;
+	END;		
 		
 END pq_ui_accueil;
 /
