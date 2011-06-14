81c82573f5f05521ddcdb5ddf823524ae914732d
david.invernizzi25@gmail.com <david.invernizzi25@gmail.com@b4689b9f-f6ca-ff5d-c3b1-f603b6193608>
2011-06-14 09:10:57 +0000

    gestion de la taille du mot de passe pour la modification du compte

--- create.js
+++ create.js
@@ -25,7 +25,7 @@ function valider(form,document){
 	firstname=form.firstname.value;
 	lastname=form.lastname.value;
 	login=form.login.value;
-	password=form.password.value;
+	pwd=form.password.value;
 	
 	if(		mail== ""
 		|| 	mail== null
@@ -35,8 +35,8 @@ function valider(form,document){
 		|| 	lastname== null
 		||	login== ""
 		|| 	login== null
-		||	password== ""
-		|| 	password== null) {			
+		||	pwd== ""
+		|| 	pwd== null) {			
 		alert("Veuillez remplir tous les champs obligatoires.");
 		return false;
 	}
@@ -44,7 +44,36 @@ function valider(form,document){
 		document.getElementById("mailText").innerHTML ="Format invalide";
 		return false;
 	}
-	else if(password.value.length!=8){
+	else if(pwd.length!=8){
+		document.getElementById("passwordText").innerHTML ="Le mot de passe doit faire 8 caractères";
+		return false;
+	}
+	return true;
+}
+
+function validerUpdCount(form,document){
+
+	mail=form.mail.value;
+	firstname=form.firstname.value;
+	lastname=form.lastname.value;
+	pwd=form.password.value;
+	
+	if(		mail== ""
+		|| 	mail== null
+		||	firstname== ""
+		|| 	firstname== null 
+		|| 	lastname== ""
+		|| 	lastname== null
+		||	pwd== ""
+		|| 	pwd== null) {			
+		alert("Veuillez remplir tous les champs obligatoires.");
+		return false;
+	}
+	else if(!verifiermail(mail)){
+		document.getElementById("mailText").innerHTML ="Format invalide";
+		return false;
+	}
+	else if(pwd.length!=8){
 		document.getElementById("passwordText").innerHTML ="Le mot de passe doit faire 8 caractères";
 		return false;
 	}
